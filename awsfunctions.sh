79f0b4b6bd0d6c1c6ae5c03dcad88a55c49606c3
Karl Roberts <karl.roberts@owtelse.com>
2014-01-25 01:57:45 +1100

    create and delete volumes oK: need to add prod tags if PROD env is set

--- awsfunctions.sh
+++ awsfunctions.sh
@@ -43,7 +43,7 @@ function message {
                           ;;
     esac
   done
-  if verifyarg "${DO_DEBUG}"; then
+  if ! verifyarg "${DO_DEBUG}"; then
     echo "$MESSAGE"
   else
     echo "$MESSAGE" >&2
@@ -110,7 +110,8 @@ function describe-thing-by-tag {
          local INFO=`ec2-describe-instances -F tag-value=${TAG} --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'`
          ;;
        "VOLUME" )
-         local INFO=`ec2-describe-volumes -F tag-value=${TAG} --region $REGION | awk '/                '${OUTPUT}'/ {print $'$COLUMN'}'`
+ #             echo "ec2-describe-volumes -F tag-value=${TAG} --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'"
+         local INFO=`ec2-describe-volumes -F tag-value=${TAG} --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'`
          ;;
        * )
          error "Oops: describe-thing-by-tag: unknown THING=$THING"
@@ -140,7 +141,8 @@ function describe-thing-by-id {
          local INFO=`ec2-describe-instances $ID --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'`
          ;;
        "VOLUME" )
-         local INFO=`ec2-describe-volumes $ID --region $REGION | awk '/                '${OUTPUT}'/ {print $'$COLUMN'}'`
+  #       echo "ec2-describe-volumes $ID --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'" >&2
+         local INFO=`ec2-describe-volumes $ID --region $REGION | awk '/'${OUTPUT}'/ {print $'$COLUMN'}'`
          ;;
        * )
          error "Oops: describe-thing-by-id: unknown THING=$THING"
@@ -212,27 +214,23 @@ function desc-instance-blockdevice-volid-by-id {
 # Get the Instance DNS addresses via a tag name
 # usage: find-instance-dns-by-tag <Tag>
 function desc-instance-dns-by-tag {
-  local TAG=$1
-  describe-instance-by-tag "INSTANCE" $TAG 4
+  describe-instance-by-tag "INSTANCE" $1 4
 }
 
 # Get the instance ID via a tag value
 # usage: desc-instance-id-by-tag <Tag>
 function desc-instance-id-by-tag {
-  local TAG=$1
-  describe-instance-by-tag "INSTANCE" $TAG 2
+  describe-instance-by-tag "INSTANCE" $1 2
 }
 
 # Get the instance AMI-ID via a tag value
 # usage: desc-instance-ami-by-tag <Tag>
 function desc-instance-ami-by-tag {
-  local TAG=$1
-  describe-instance-by-tag "INSTANCE" $TAG 3
+  describe-instance-by-tag "INSTANCE" $1 3
 }
 
 function desc-instance-status-by-tag {
-  local TAG=$1
-  describe-instance-by-tag "INSTANCE" $TAG 6
+  describe-instance-by-tag "INSTANCE" $1 6
 }
 
 function desc-instance-blockdevice-name-by-tag {
@@ -272,25 +270,25 @@ function desc-volume-status-by-id {
 #############################################################
 # Volume descriptions                                       #
 #############################################################
-# usage: describe-volume-by-tag $1 $2 $3                    #
+# usage: describe-volumes-by-tag $1 $2 $3                    #
 # $1 = <VOLUME|ATTACHMENT|TAG>                              #
 # $2 = <TAGVALUE>                                           #
 # $3 = <OUTPUT-COL-NUM>                                     #
 #############################################################
-function describe-volume-by-tag {
+function describe-volumes-by-tag {
   describe-thing-by-tag "VOLUME" "$1" "$2" "$3"
 }
 
 function desc-volume-id-by-tag {
-  describe-volume-by-tag "VOLUME" $1 2
+  describe-volumes-by-tag "VOLUME" $1 2
 }
 
 function desc-volume-size-by-tag {
-  describe-volume-by-tag "VOLUME" $1 3
+  describe-volumes-by-tag "VOLUME" $1 3
 }
 
 function desc-volume-status-by-tag {
-  describe-volume-by-tag "VOLUME" $1 6
+  describe-volumes-by-tag "VOLUME" $1 6
 }
 
 ###########################################################
@@ -300,22 +298,46 @@ function desc-volume-status-by-tag {
 # Create Volume
 # VOLUME=`ec2-create-volume -s 50 --region ${REGION} -z ${ZONE} | awk '/VOLUME/ {print $2}'`
 #############################################################
+# create volume and tag it. return the volID
 function create-volume {
-  local ID=`ec2-create-volume $@ --region ${REGION} -z ${ZONE} | awk '/VOLUME/ {print $2}'`
-  message ID
+  local TAG=$1; shift
+  local SIZE=$1; shift
+  verifyarg $TAG && verifyarg $SIZE || error "Error:create-volume: TAG=$TAG SIZE=$SIZE"
+  local VOLUME=`ec2-create-volume --size ${SIZE} --region ${REGION} -z ${ZONE} | awk '/VOLUME/ {print $2}'`
+  local TAGDETAILS=`ec2-create-tags ${VOLUME} -t Name=${TAG} --region ${REGION}`
+  message $VOLUME
+}
+function create-snapshot {
+  local TAG=$1; shift
+  local FROM=$1; shift
+  local DESC=$1; shift
+  verifyarg "$TAG" && verifyarg "$FROM" && verifyarg "$DESC" || error "Error:create-snapshot: TAG=$TAG FROM=$FROM DESC=\"$DESC\""
+  TAG="-${TAG}"
+  local FROMIDs=`desc-volume-id-by-tag $FROM`
+  for F in $FROMIDs; do 
+    local SNAP=`ec2-create-snapshot --region ${REGION} -d "${DESC}" "${F}" | awk '/SNAPSHOT/ {print $2}'`
+    local TAGDETAILS=`ec2-create-tags ${SNAP} -t Name=snapshot-${F}${TAG} --region ${REGION}`
+    message "$SNAP" 
+  done
+}
+function create-sized-volume {
+  local TAG=$1
+  local SIZE=$2
+  verifyarg $TAG && verifyarg $SIZE || error "Error:create-50G-volume: TAG=$TAG SIZE=$SIZE"
+  create-volume $TAG $SIZE
 }
 function create-50G-volume {
-  create-volume -s 50
+  create-sized-volume $1 50
 }
 function delete-volume-by-id {
   local ID=$1
-  verifyarg $ID
-  local THEID=`ec2-delete-volume $ID`
+  verifyarg $ID  || error "Error:delete-volume-by-id: No ID supplied"
+  local THEID=$(ec2-delete-volume --region ${REGION} $ID | awk '/VOLUME/ {print $2}')
   message $THEID
 }
 function delete-volume-by-tag {
   local TAG=$1
-  verifyarg $TAG
+  verifyarg $TAG || error "Error:delete-volumes-by-tag: No TAG supplied"
   local IDs=`desc-volume-id-by-tag $TAG`
   for id in $IDs; do
     local i=`delete-volume-by-id $id`
